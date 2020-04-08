7b4e3aa0022003e4a77efc5bd23519c0b6cc4dac
Ashkan <ashkan_ss@hotmail.com>
2020-04-08 10:46:37 -0400

    Changed to queyry

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const request = require('request');
 
 
 const token = 'AIzaSyC-ukkQ3n0lrfQ3IBe5HEMgomaP2hUOMJk';
-const lat = -17.3836317;
+/*const lat = -17.3836317;
 const lng = -66.2658546;
-const radius = 5000;
+const radius = 5000;*/
+const city='Cochabamba';
 const GOOGLE_PAGINATION_DELAY = 1000;
 var nextPage = '';
 var url;
@@ -14,7 +15,8 @@ var hospitals = [];
 getHospitals();
 
 function getHospitals() {
-    url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=hospital&key=${token}&pagetoken=${nextPage}`;
+    //url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&keyword=hospital%clinica%centro&type=hospital&key=${token}&pagetoken=${nextPage}`;
+    url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=hospitals+in+${city}&key=AIzaSyC-ukkQ3n0lrfQ3IBe5HEMgomaP2hUOMJk&pagetoken=${nextPage}`;
     request({url: url}, (error, response) => {
         let data = JSON.parse(response.body);
         data.results.forEach(place => {
